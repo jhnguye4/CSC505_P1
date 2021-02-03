0031a77ef50cb0439542e046843c491e43109e38
Justin Kirschner <jkirsch@ncsu.edu>
2021-02-02 20:19:12 -0500

    inital list

--- main.cpp
+++ main.cpp
@@ -10,8 +10,11 @@ int main(int argc, const char * argv[]) {
    
     ifstream ifs (ifile); // make filestream for input file
            
-           while (ifs.good()){ /
+           while (ifs.good()){
                while (getline(ifs, num)){ // one int per line
+                   
+                   list<int> all_nodes;
+                   all_nodes.push_back(num);
 
                }
            }
