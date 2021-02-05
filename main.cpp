edcd3af869723678aa1e6dedf2874523eea67abd
Justin Kirschner <jkirsch@ncsu.edu>
2021-02-05 08:58:31 -0500

    quicksort 1st compile, needs debug

--- main.cpp
+++ main.cpp
@@ -7,8 +7,75 @@
 #include <stdlib.h>
 #include<stdio.h>
 #include <sstream>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
+
+
+
+list<int>::iterator partition(list<int> my_list, list<int>::iterator start, list<int>::iterator end){
+    cout << "hello from partition" << endl;
+    
+    list<int>::iterator _start = start;
+    list<int>::iterator _end = end;
+    cout << "after declarations" << endl;
+    int max = *max_element(_start,_end);
+    int min = *min_element(_start,_end);
+    cout << "hello min max" << endl;
+    int pivot =  ( std::rand() % ( max - min + 1 ) );
+    cout << "hello pivot" << endl;
+    
+    cout <<  *my_list.begin() << endl;
+    cout <<  *_start << end;
+    cout <<  *_end << endl;
+    
+    
+    cout <<  *distance(my_list.begin(),_start) << endl;
+    cout <<  *distance(my_list.begin(),_end ) << end;
+    
+    while(distance(my_list.begin(),_start) <= distance(my_list.begin(),_end)){
+        cout << "hello from partition while loop 1" << endl;
+        while((distance(my_list.begin(),_start) <= distance(my_list.begin(),_end)) && (*_start < pivot)){
+            next(_start,1);
+        }
+        
+        while((distance(my_list.begin(),_start) <= distance(my_list.begin(),_end)) && (*_end > pivot)){
+            cout << "hello from partition while loop 2" << endl;
+            advance(_end, -1);
+        }
+        
+        if (*_start < *_end){
+            cout << "hello from partition if loop" << endl;
+            swap(*_start,*_end);
+            advance(_start, 1);
+            advance(_end, -1);
+        }
+        
+        swap(*start, *_end);
+    }
+    
+    return end;
+    
+}
+
+void quicksort(list<int> my_list, list<int>::iterator start, list<int>::iterator end){
+    
+    
+    list<int>::iterator begin_it = my_list.begin();
+    list<int>::iterator end_it = next(my_list.end(),-1);
+    
+    
+    list<int>::iterator pivot = partition(my_list, begin_it, end_it);
+    list<int>::iterator pivot1 = next(pivot,1);
+    
+    quicksort(my_list,my_list.begin(), pivot); // is my_list updated tho?
+    quicksort(my_list,pivot1, next(my_list.end(),-1));
+    
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
    
@@ -27,6 +94,12 @@ int main(int argc, const char * argv[]) {
         all_nodes.push_back(num); //add num to end of list
     }
     
+    for (list<int>::iterator iter = all_nodes.begin(); iter != all_nodes.end(); ++iter){
+        cout << *iter << endl; //
+    }
+    
+    quicksort(all_nodes, all_nodes.begin(), next(all_nodes.end(),-1));
+    
     //for (auto const& n: all_nodes) {cout << n << endl;} C++11
     
     //Print final list
@@ -35,7 +108,6 @@ int main(int argc, const char * argv[]) {
     for (list<int>::iterator iter = all_nodes.begin(); iter != all_nodes.end(); ++iter){
         cout << *iter << endl; //
     }
-    
     
     
 } // end main
