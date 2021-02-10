416b0178a82c7818e0834d13b90c1595d8f6e904
jhnguye4 <jhnguye4@ncsu.edu>
2021-02-09 22:05:32 -0500

    Added comments to insertion sort and cleaned up code

--- Isort.java
+++ Isort.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class Is {
+public class Isort {
     LinkedList<Integer> list = new LinkedList<Integer>();
 
-    public Is() {
+    public Isort() {
         list.addFirst(11);
         list.addFirst(44);
         list.addFirst(22);
@@ -15,37 +15,52 @@ public class Is {
         list.addFirst(35);
         list.addFirst(99);
         list.addFirst(77);
-        System.out.println(list);
+        System.out.println("List Before Sort: " + list);
         sort();
-        System.out.println(list);
-
+        System.out.println("List After Sort: " + list);
     }
 
     public static void main(String[] args) {
-        new Is();
+        new Isort();
     }
 
-
-
+    /*
+     * The idea of insertion sort is that it will go through each element in the
+     * list. At each element, it will then look at each element backwards to see if
+     * it is less. It will keep moving back for each element it is less than and
+     * then be inserted at the final position where it will shift all the other
+     * elements down. This algorithm is normally time consuming if you are using an
+     * array and need to shift each element down the array to insert, for this
+     * situation we used a linkedlist so all we had to do was unlink the pointers
+     * and relink it where it needed to be inserted. This algorithms worst case is
+     * O(n^2), and is normally used for smaller data sets.
+     */
     public void sort() {
-
+        // Declared i as 1 since we cant compare any elements before the first element.
         int i = 1;
-        //System.out.println(list);
-        while (i <= list.size()-1) {
-            
+
+        // Going through each element of the array from the second element to the end
+        while (i < list.size()) {
+
+            // Counter that will be used to see how many elements back we have to move the
+            // element at i
             int count = 0;
-            for (int k = i-1; k >= 0; --k){    
-                if (list.get(i) < list.get(k)){
-                ++count;
+
+            // k is set to the index before i and will be decremented till it reaches the
+            // first element.
+            for (int k = i - 1; k >= 0; --k) {
+                // Count will be incremented when value at k is greater than the value we are
+                // trying to insert.
+                if (list.get(i) < list.get(k)) {
+                    ++count;
                 }
             }
 
+            // Procedure in order to unlink insertion value and move it an x amount back.
             int tmp = list.get(i);
             list.remove(i);
-            list.add(i-count,tmp);
-            System.out.println(list);
+            list.add(i - count, tmp);
             ++i;
-            
         }
     }
 }
