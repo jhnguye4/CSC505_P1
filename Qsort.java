2c2c459defebe8cc1a624870911f44a8e4435a77
jhnguye4 <jhnguye4@ncsu.edu>
2021-02-09 23:10:42 -0500

    IO implemented for all sorting algorithms

--- Qsort.java
+++ Qsort.java
@@ -1,28 +1,123 @@
 import java.util.*;
+import java.io.*;
 
 public class Qsort {
     LinkedList<Integer> list = new LinkedList<Integer>();
 
     public Qsort() {
-        list.addFirst(11);
-        list.addFirst(44);
-        list.addFirst(22);
-        list.addFirst(55);
-        list.addFirst(77);
-        list.addFirst(0);
-        list.addFirst(77);
-        list.addFirst(5);
-        list.addFirst(35);
-        list.addFirst(99);
-        list.addFirst(77);
-        System.out.println("List Before Sort: " + list);
-        sort(0, list.size() - 1);
-        System.out.println("List After Sort: " + list);
+        // Asking user to input a text file that ends with input.txt and with a single
+        // integer on each line
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter a filename or Q to quit: ");
+        String filename = console.next().toLowerCase();
+
+        Scanner input = null;
+        PrintStream output = null;
+        // Will constantly ask user for input files that end with input.txt to sort
+        // numbers. Will reprompt user for valid file or type q to quit
+        while (!(filename.equals("q"))) {
+            if (filename.endsWith("input.txt")) {
+                // Scanner for input file
+                input = getInputScanner(filename);
+                if (input != null) {
+                    // Calls function to print the sorted list into output file
+                    output = getOutputPrintStream(console, filename);
+                    if (output != null) {
+                        // Function that goes into input file and puts each integer per line into global
+                        // list
+                        process(input);
+                        System.out.println("List Before Sort: " + list);
+                        sort(0, list.size() - 1);
+                        System.out.println("List After Sort: " + list);
+
+                        for (int i = 0; i < list.size(); i++) {
+                            output.println(list.get(i));
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Invalid filename");
+            }
+            System.out.print("Enter a filename or Q to quit: ");
+            filename = console.next().toLowerCase();
+        }
 
     }
 
     public static void main(String[] args) {
         new Qsort();
+    }
+
+    // Returns Scanner for an input file
+    // Returns null if the file does not exist
+    /**
+     * getInputScanner method is used to create a scanner of the input file the user
+     * entered. It will return null and a FileNotFoundException if the user enters a
+     * file that does not exist.
+     *
+     */
+    public Scanner getInputScanner(String filename) {
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return fileScanner;
+    }
+
+    // Returns PrintStream for an output file
+    // If the output file already exists, asks the user if it is OK to overwrite the
+    // file
+    // If it is not OK to overwrite the file or a FileNotFoundException occurs, null
+    // is
+    // returned instead of a PrintStream
+    /**
+     * getOutputPrintStream method is used to create a output files with the
+     * opposite extentions. If the file already exists then it will prompt the user
+     * if they want to overwrite the file If they respond no then the output will
+     * return null. If file is unable to be written then it will prompt the user of
+     * the error
+     *
+     */
+    public PrintStream getOutputPrintStream(Scanner console, String filename) {
+        PrintStream output = null;
+        if (filename.endsWith("input.txt")) {
+            filename = filename.substring(0, filename.length() - 9);
+            filename = filename + "output.txt";
+
+        }
+        File file = new File(filename);
+        try {
+            if (!file.exists()) {
+                output = new PrintStream(file);
+            } else {
+                System.out.print(filename + " exists - OK to overwrite(y,n)?: ");
+                String reply = console.next().toLowerCase();
+                if (reply.startsWith("y")) {
+                    output = new PrintStream(file);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File unable to be written " + e);
+        }
+        return output;
+    }
+
+    /*
+     * process function takes in the input scanner and the goes line by line placing
+     * the integer into the global list
+     */
+    public void process(Scanner input) {
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            Scanner lineScan = new Scanner(line);
+            if (lineScan.hasNextInt()) {
+                list.addFirst(lineScan.nextInt());
+            }
+            lineScan.close();
+        }
+
     }
 
     // Function that swaps values at the two indexes of the global list.
