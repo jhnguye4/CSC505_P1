5f26aa1f60f3341e37f5efa6879a3b5ae0a78fb7
jhnguye4 <jhnguye4@ncsu.edu>
2021-02-19 10:30:38 -0500

    edited IO and ran algos on inputs

--- Isort.java
+++ Isort.java
@@ -3,112 +3,29 @@ import java.io.*;
 
 public class Isort {
     private ListNode head = null;
-    private int numberComparisons = 0;
+    private long numberComparisons = 0;
 
-    public Isort(String filename) {
-        filename = filename.toLowerCase();
+    public Isort() {
+        Scanner input = new Scanner(System.in);
+        while(input.hasNext()){
+            add(input.nextInt());
+        }
 
-        Scanner input = null;
-        PrintStream output = null;
-
-        input = getInputScanner(filename);
-        if (input != null) {
-            // Calls function to print the sorted list into output file
-            output = getOutputPrintStream(filename);
-            if (output != null) {
-                // Function that goes into input file and puts each integer per line into global
-                // list
-                process(input);
-
-                long start = System.nanoTime();
-                sort();
-                long end = System.nanoTime();
-                long sortTimeInNano = end - start;
-                double sortTimeIn10thSeconds = (double) sortTimeInNano / Math.pow(10, 8);
-                System.err.println("Time after sorting list in 10th of second: " + sortTimeIn10thSeconds);
-                System.err.println("Number of Comparisons: " + numberComparisons);
-
-                output.println("Time after sorting list in 10th of second: " + sortTimeIn10thSeconds);
-                output.println("Number of Comparisons: " + numberComparisons);
-                while (head != null) {
-                    output.println(head.data);
-                    head = head.next;
-                }
-            }
+        long start = System.nanoTime();
+        sort();
+        long end = System.nanoTime();
+        long sortTimeInNano = end - start;
+        double sortTimeIn10thSeconds = (double) sortTimeInNano / Math.pow(10, 8);
+        System.err.println("Time after sorting list in 10th of second: " + sortTimeIn10thSeconds);
+        System.err.println("Number of Comparisons: " + numberComparisons);
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
         }
     }
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            new Isort(args[0]);
-        } else {
-            System.out.println("Please add input file to be sorted at command line.");
-        }
-
-    }
-
-    // Returns Scanner for an input file
-    // Returns null if the file does not exist
-    /**
-     * getInputScanner method is used to create a scanner of the input file the user
-     * entered. It will return null and a FileNotFoundException if the user enters a
-     * file that does not exist.
-     *
-     */
-    public Scanner getInputScanner(String filename) {
-        Scanner fileScanner = null;
-        try {
-            fileScanner = new Scanner(new File(filename));
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        return fileScanner;
-    }
-
-    // Returns PrintStream for an output file
-    // If the output file already exists, asks the user if it is OK to overwrite the
-    // file
-    // If it is not OK to overwrite the file or a FileNotFoundException occurs, null
-    // is
-    // returned instead of a PrintStream
-    /**
-     * getOutputPrintStream method is used to create a output files with the
-     * opposite extentions. If the file already exists then it will prompt the user
-     * if they want to overwrite the file If they respond no then the output will
-     * return null. If file is unable to be written then it will prompt the user of
-     * the error
-     *
-     */
-    public PrintStream getOutputPrintStream(String filename) {
-        PrintStream output = null;
-        if (filename.endsWith("input.txt")) {
-            filename = filename.substring(0, filename.length() - 9);
-            filename = filename + "Isort_output.txt";
-
-        }
-        File file = new File(filename);
-        try {
-            output = new PrintStream(file);
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File unable to be written " + e);
-        }
-        return output;
-    }
-
-    /*
-     * process function takes in the input scanner and the goes line by line placing
-     * the integer into the global list
-     */
-    public void process(Scanner input) {
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            Scanner lineScan = new Scanner(line);
-            if (lineScan.hasNextInt()) {
-                add(lineScan.nextInt());
-            }
-            lineScan.close();
-        }
+        new Isort();
     }
 
     /*
@@ -119,9 +36,16 @@ public class Isort {
      * elements down. This algorithm is normally time consuming if you are using an
      * array and need to shift each element down the array to insert, for this
      * situation we used a linkedlist so all we had to do was unlink the pointers
-     * and relink it where it needed to be inserted. This algorithms worst case is
-     * O(n^2), and is normally used for smaller data sets.
-     */
+     
+
+         */
+    
+    
+    
+    
+    
+
+    
     public void sort() {
         // Getting second node of list since we cant compare any elements before the
         // first node.
